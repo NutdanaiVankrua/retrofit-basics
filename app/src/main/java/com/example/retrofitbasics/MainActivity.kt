f945973d46bf6c47a8fc3852c74eeb5d5d4e1fda
Nutdanai Vankrua <nutdanai@odd.works>
2020-07-16 09:05:02 +0700

    example different color for button

--- app/src/main/java/com/example/retrofitbasics/MainActivity.kt
+++ app/src/main/java/com/example/retrofitbasics/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         call_service_button.setOnClickListener {
+            call_service_button.isSelected = call_service_button.isSelected.not()
             val greetingRequest = dataStore
                 .request()
                 .subscribeOn(Schedulers.io())
